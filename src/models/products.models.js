f2eeba9fe96162d8c675d2d418b5d1c706909b8a
Jainen B Albornoz <jainenbanqueroalbornoz@gmail.com>
2025-07-23 18:12:11 -0300

    getProductById

--- src/models/products.models.js
+++ src/models/products.models.js
@@ -1,6 +1,6 @@
 import { db } from './firebase.js';
 
-import {collection, getDocs} from 'firebase/firestore';
+import {collection, getDocs, doc, getDoc} from 'firebase/firestore';
 
 // Function to get all products
 
@@ -15,9 +15,24 @@ export const getAllProducts = async () => {
             ...doc.data(),
         }));
         return products;
-    } catch (error) { 
+    } catch (error) {
         console.error("Error fetching products:", error);
         throw new Error("Could not fetch products");
     }
     
+};
+
+export const getProductById = async (id) => {
+    try {
+        const docRef = doc(productsCollection, id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            return { id: docSnap.id, ...docSnap.data() };
+        } else {
+            return null; // Product not found
+        }
+    } catch (error) {
+        console.error("Error fetching product by ID:", error);
+        throw new Error("Could not fetch product");
+    }
 }
--- src/services/products.service.js
+++ src/services/products.service.js
@@ -2,4 +2,10 @@ import * as Model from "../models/products.models.js";
 
 export const getAllProducts = async () => {
     return await Model.getAllProducts();
-}
+};
+
+export const getProductById = async (id) => {
+    return await Model.getProductById(id);
+};
+
+
