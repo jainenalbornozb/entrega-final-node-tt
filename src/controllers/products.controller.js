be89ecdffa112d2f6eebcade8f2adde477cbec24
Jainen B Albornoz <jainenbanqueroalbornoz@gmail.com>
2025-07-24 00:18:27 -0300

    añadí createProduct, updateProduct y deleteProduct

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -14,3 +14,37 @@ export const getProductById = async (req, res) => {
         res.status(404).json({ message: 'Product not found' });
     }
 }
+
+export const createProduct = async (req, res) => {
+  const { name, price, categories } = req.body;
+
+  const product = await Model.createProduct({ name, price, categories });
+
+  res.status(201).json(product);
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, categories } = req.body;
+
+  const updatedProduct = await Model.updateProduct(id, { name, price, categories });
+
+  if (!updatedProduct) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  res.json(updatedProduct);
+};
+
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  const deleted = await Model.deleteProduct(id);
+
+  if (!deleted) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  res.status(204).send();
+};
--- src/models/products.models.js
+++ src/models/products.models.js
@@ -1,6 +1,14 @@
 import { db } from './firebase.js';
 
-import {collection, getDocs, doc, getDoc} from 'firebase/firestore';
+import {
+    collection,
+    getDocs,
+    doc,
+    getDoc,
+    addDoc,
+    setDoc,
+    deleteDoc
+} from 'firebase/firestore';
 
 // Function to get all products
 
@@ -35,4 +43,37 @@ export const getProductById = async (id) => {
         console.error("Error fetching product by ID:", error);
         throw new Error("Could not fetch product");
     }
+    
 }
+
+export const createProduct = async (product) => {
+    try {
+        const docRef = await addDoc(productsCollection, newProduct);
+        return { id: docRef.id, ...newProduct};
+    } catch (error) {
+        console.error("Error adding product:", error);
+        throw new Error("Could not add product");
+    }
+}
+
+export const updateProduct = async (id, updatedProductData) => {
+  try {
+    const docRef = doc(productsCollection, id);
+    await setDoc(docRef, product);
+    return { id, ...updatedProductData };
+  } catch (error) {
+    console.error("Error updating product:", error);
+    throw new Error("Could not update product");
+  }
+};
+
+export const deleteProduct = async (id) => {
+  try {
+    const docRef = doc(productsCollection, id);
+    await deleteDoc(docRef);
+    return { id };
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    throw new Error("Could not delete product");
+  }
+};
